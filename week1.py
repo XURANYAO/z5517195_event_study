0029b33ce0e6d31440cc139b5cc71f1c9751994e
XURANYAO <z5517195@ad.unsw.edu.au>
2024-04-03 09:55:19 +1100

    move week 1 to repository

---
+++ week1.py
@@ -0,0 +1,11 @@
+
+import yfinance                                           # (1)
+tic = "QAN.AX"                                            # (2)
+start = '2020-01-01'                                      # (3)
+end = None                                                # (4)
+df = yfinance.download(tic, start, end, ignore_tz=True)   # (5)
+print(df)                                                 # (6)
+df.to_csv('qan_stk_prc.csv')
+
+print("Welcome to the Tooklit for Finance T1 2024, Hello from XURAN YAO!")
+
